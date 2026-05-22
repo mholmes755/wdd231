cdf810107c129193f4f211de7479b9613a00b2e7
Michael Holmes <littleholmes2019@gmail.com>
2026-05-22 06:46:52 -0600

    created weather lab for week 03

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,7 +1,12 @@
-const weatherIcon = document.querySelector("#weather-icon");
-const currentTemp = document.querySelector('#current-temp');
-const caption = document.querySelector('figcaption');
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.63&appid=a5ec3c1a728334bf4f2bc7a0d0972919&lang=en&units=imperial';
+const weatherIcon = document.querySelector("#weatherIcon");
+const currentTemperature = document.querySelector('#temperature');
+const caption = document.querySelector('#caption');
+const town = document.querySelector('#town');
+
+const mykey = "a5ec3c1a728334bf4f2bc7a0d0972919"
+const long = 6.63;
+const lat = 49.75;
+const url = `//api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${mykey}&lang=en&units=imperial`;
 
 
 
@@ -13,6 +18,7 @@ async function apiFetch() {
         if (response.ok) {
             const data = await response.json();
             console.log(data); // Testing the API response
+            displayResults(data);
         } else {
             throw Error(await response.text());
         }
@@ -20,6 +26,18 @@ async function apiFetch() {
         console.log(error);
     }
 }
+
+// Display JSON data
+
+function displayResults(data) {
+    town.innerHTML = data.name;
+    caption.innerHTML = data.weather[0].description;
+    currentTemperature.innerHTML = `${data.main.temp}&degF`;
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    weatherIcon.setAttribute('SRC', iconsrc);
+    weatherIcon.setAttribute('alt', data.weather[0].description);
+}
+
 
 
 apiFetch();
--- week03/scripts/course.mjs
+++ week03/scripts/course.mjs
@@ -2,9 +2,21 @@ const byuiCourse = {
     code: "WDD231",
     name: "Web Frontend Development I",
     sections: [
-        { sectionNumber: 1, enrolled: 88, instructor: "Brother Bingham", },
-        { sectionNumber: 2, enrolled: 81, instructor: "Sister Shultz", },
-        { sectionNumber: 3, enrolled: 95, instructor: "Sister Smith", },
+        {
+            sectionNumber: 1,
+            enrolled: 88,
+            instructor: "Brother Bingham",
+        },
+        {
+            sectionNumber: 2,
+            enrolled: 81,
+            instructor: "Sister Shultz",
+        },
+        {
+            sectionNumber: 3,
+            enrolled: 95,
+            instructor: "Sister Smith",
+        },
     ],
     changeEnrollment: function (sectionNumber, add = true) {
         // Find the section with the given section number
